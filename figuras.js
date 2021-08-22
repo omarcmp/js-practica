a9fb6d567d1db80a1a288546449e7ee64b78fcf9
Omar Mamani <omar.cmp@gmail.com>
2021-08-22 10:40:07 -0500

    ejemplo

---
+++ figuras.js
@@ -0,0 +1,8 @@
+console.group("Cuadrado");
+const ladoCuadrado = 5;
+console.log("Los lados del cuadrado miden: "+ladoCuadrado);
+const perimetroCuadrado = ladoCuadrado*4;
+console.log("El perímetro de un cuadrado es: "+perimetroCuadrado);
+const areaCuadrado = ladoCuadrado*ladoCuadrado;
+console.log("El área de un cuadrado es: "+areaCuadrado);
+console.groupEnd();
